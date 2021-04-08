650c94218ff866fb25384eda5de2932708210a6b
张吉南 <844060885@qq.com>
2021-04-08 19:16:41 +0800

    星云项目

--- nebulaJS.js
+++ nebulaJS.js
@@ -1,3 +1,18 @@
 window.onload = function(){
-    new Vue({el: '#title',data: {massage: 'Nebula'}})
+    new Vue({el: '#title',data: {massage: 'Nebula'}});
+    var element = {
+        name: "id-article-right-top",
+        width: "100px",
+        heigth: "100px"
+    }
+    Click_deformation ( element );
+}
+function Window_alert ( text ) {
+    alert ( text );
+}
+function Click_deformation ( c_element ) {
+    document.getElementById ( c_element.name ).style.width = c_element.width;
+    document.getElementById ( c_element.name ).style.height = c_element.height;
+}
+function Element_deformation ( c_element ) {
 }
